38d143ead021dbd7c324f023d71662e3e54dcf57
atomicjuice <keemowatson1@gmail.com>
2020-10-21 23:51:51 +0100

    added more details to the header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Main from './Components/main'
 import Navbar from './Components/navbar'
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
