ce1c45232edcb2821665b8ad2020ec0084e9cf76
atomicjuice <keemowatson1@gmail.com>
2020-10-23 15:24:44 +0100

    building out the about section

--- src/Components/Sections/aboutme.js
+++ src/Components/Sections/aboutme.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import '../../myCSS/aboutme.css';
 
-class AboutMe extends React.Component{
-    render(){
-        return(<div>
-            <button href='#top' >
-                this takes you to the topD
-            </button>
-        </div>)
+class AboutMe extends React.Component {
+    render() {
+        return (
+            <div id='aboutMeContainer'>
+                <p>
+                    Hi, I'm   
+                </p>
+                <h1>
+                    Keemo,
+                </h1>
+                <h1>
+                    and I build things online
+                </h1>
+                <p>
+                    
+                </p>
+            </div>
+        )
     }
 }
 
-export default AboutMe ;
+export default AboutMe;
