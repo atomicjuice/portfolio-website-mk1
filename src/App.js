78ded53d00e231c427a2673f7eeb95601a565901
atomicjuice <keemowatson1@gmail.com>
2020-10-31 20:59:25 +0000

    cleaned up the code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import Main from './Components/main'
 import Navbar from './Components/navbar'
 import Aboutme from './Components/Sections/aboutme';
-// import './App.css';
+import './App.css';
 
-class App extends Component {
-  state = {
-    screenWidth: window.screen.width
-  }
+function App() {
 
-  forPhone = () => {
-    if (this.state.screenWidth <= 400){
-      return <Navbar id="mobile" sticky="top" /> 
-    }
-  }
-  render(){
   return (
     <div className='App' >
-      {this.forPhone()}
-      {/* <Navbar sticky="top" /> */}
-      {/* <Main/> */}
+      <Navbar sticky="top" />
       <Aboutme/>
-    </div>
-  );
-}}
+    </div>)
+}
 
 export default App;
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -12,9 +12,9 @@ class NavBar extends React.Component {
       <Navbar variant="dark" sticky='top' id='extraStuff' >
         <Navbar.Brand href="#home" id='brand' >Navbar</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home" id='navlink1' className='navlink' >1. About</Nav.Link>
-          <Nav.Link href="#features" id='navlink2' className='navlink' >2. Projects</Nav.Link>
-          <Nav.Link href="#pricing" id='navlink3' className='navlink' >3. Contact</Nav.Link>
+          <Nav.Link href="#home" className='navlink' >1. About</Nav.Link>
+          <Nav.Link href="#features" className='navlink' >2. Projects</Nav.Link>
+          <Nav.Link href="#pricing" className='navlink' >3. Contact</Nav.Link>
         </Nav>
       </Navbar>
     )
